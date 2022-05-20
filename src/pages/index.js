35b8e101c686dda8417bae48d8d35af43a7ecfb4
Bruno Martins <gz4256@wayne.edu>
2022-05-20 16:27:52 -0400

    Fix Image on Part3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { staticImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="BCM">
       <p>Simple Blog using Gatsby</p>
-      <img src={"./src/images/IMG_0475.PNG"} alt="Space" />
+      <StaticImage
+      alt="Main Image"
+      src={"../images/IMG_0475.PNG"} alt="Space" 
+      />
     </Layout>
   )
 }
